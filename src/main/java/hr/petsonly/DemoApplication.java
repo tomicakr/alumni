9914ddd6742900764d04fc1c1ed40f6199af05f8
Mate Paulinovic <mate.paulinovic@fer.hr>
2018-01-07 00:53:52 +0100

    Dodano jos primjera rezervacija

--- src/main/java/hr/petsonly/DemoApplication.java
+++ src/main/java/hr/petsonly/DemoApplication.java
@@ -9,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 10; i++){
-			System.out.println(UUID.randomUUID());
-		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }

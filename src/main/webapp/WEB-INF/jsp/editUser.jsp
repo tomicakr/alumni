d551a996e3a07a9b03e7153657ec0f89cdb9176a
Filip Sodić <filip.sodic@fer.hr>
2018-01-06 22:14:42 +0100

    neke kombe s modalom

---
+++ src/main/webapp/WEB-INF/jsp/editUser.jsp
@@ -0,0 +1,70 @@
+<!--<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<jsp:include page="../partials/head.jsp" flush="true">
+    <jsp:param name="title" value="Registracija" />
+    <jsp:param name="view-name" value="register" />
+</jsp:include>
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit user</title>
+</head>
+<body>
+<main class="ui middle aligned center aligned grid">
+    <div class="column">
+        <h1 class="ui image massive header">
+            Izradite svoj račun
+        </h1>
+        <form:form action="/users/" method="post" class="ui large form segment stacked" modelAttribute="user">
+            <div class="two fields">
+                <div class="field">
+                    <input type="text" name="name" id="first-name" placeholder="Ime">
+                </div>
+                <div class="field">
+                    <input type="text" name="surname" id="last-name" placeholder="Prezime">
+                </div>
+            </div>
+            <div class="field">
+                <input type="text" name="userPid" id="oib" placeholder="OIB" disabled>
+            </div>
+            <div class="field">
+                <input type="tel" name="mobilePhone" id="mobile-phone" placeholder="Broj mobitel">
+            </div>
+            <div class="field">
+                <input type="tel" name="phone" id="telephone" placeholder="Broj telefona" disabled>
+            </div>
+            <div class="field">
+                <input type="email" name="email" id="email" placeholder="Adresa elektroničke pošte" disabled>
+            </div>
+            <div class="field">
+                <select name="location" title="">
+                    <option value="10000" selected>Zagreb</option>
+                    <option value="21000">Split</option>
+                    <option value="31000">Osijek</option>
+                    <option value="42000">Varaždin</option>
+                </select>
+            </div>
+            <div class="field">
+                <input type="text" name="address" id="address" placeholder="Adresa stavnovanja" disabled>
+            </div>
+            <div class="field">
+                <input type="old-password" name="old-password" id="old-password" placeholder="Unesite staru lozinku">
+            </div>
+            <div class="field">
+                <input type="password" name="password" id="password" placeholder="Unesite novu lozinku">
+            </div>
+            <div class="field">
+                <input type="password" name="password2" id="password2" placeholder="Ponovite novu lozinku">
+            </div>
+            <i id="pass-match" class="fa large" aria-hidden="true"></i>
+            <div class="ui fluid huge darkred submit button" style="width: 100%">Registriraj se!</div>
+        </form:form>
+    </div>
+</main>
+<script src="${pageContext.request.contextPath}/scripts/register.js">
+</script>
+</body>
+</html>
--- src/main/webapp/scripts/profile.js
+++ src/main/webapp/scripts/profile.js
@@ -1,41 +1,44 @@
-const userUrl =window.location.href + (window.location.href.endsWith('/') ? '' : '/');
+const userUrl = window.location.href + (window.location.href.endsWith('/') ? '' : '/');
 const reservationsTable = $('#reservations').find('table');
 const petsTable = $('#pets').find('table');
 const btnPets = $('#btn-pets');
 const btnReservations = $('#btn-reservations');
 const btnEdit = $('#btn-edit');
 const btnDelete = $('#btn-delete');
+const btnAddPet = $('#btn-add-pet');
+const btnAddReservation = $('#btn-add-reservation');
+const addPetForm = $('#add-pet-form');
 
-function formatTableRow (...cells){
+function formatTableRow(...cells) {
     let result = '<tr>';
     result += cells.map(cell => `<td>${cell}</td>`).join('');
-    result +='</tr>';
+    result += '</tr>';
     return result;
 }
 
-function appendReservation(res){
-    let markup = $(formatTableRow(res.pet,res.service,res.employee,res.status,res.time));
+function appendReservation(res) {
+    let markup = $(formatTableRow(res.pet, res.service, res.employee, res.status, res.time));
     reservationsTable.append(markup);
 }
 
-function appendPet(pet){
-    let markup = $(formatTableRow(pet.name,pet.age,pet.species,pet.breed,pet.sex,pet.microchip,pet.remark));
+function appendPet(pet) {
+    let markup = $(formatTableRow(pet.name, pet.age, pet.species, pet.breed, pet.sex, pet.microchip, pet.remark));
     petsTable.append(markup);
 }
 
 let getDataOn = entities => $.getJSON(`${userUrl}${entities}/`);
 
-function updateReservatoins(){
+function updateReservatoins() {
     reservationsTable.empty();
     updateTable(getDataOn('reservations'), appendReservation)
 }
 
-function updatePets(){
+function updatePets() {
     petsTable.empty();
-    updateTable(getDataOn('pets'),appendPet)
+    updateTable(getDataOn('pets'), appendPet)
 }
 
-function updateTable(jsonGetter,appender){
+function updateTable(jsonGetter, appender) {
     return jsonGetter
         .then(entities => {
             entities.forEach(entity => appender(entity))
@@ -44,17 +47,53 @@ function updateTable(jsonGetter,appender){
 }
 
 btnPets.click(updatePets);
+btnAddPet.click(function() {
+    $('.ui.modal')
+        .modal('show');
+});
 btnReservations.click(updateReservatoins);
-btnDelete.click(function(){
-   let decision = confirm("Jeste li sigurni da želite izbrisati korisnika?");
-   if(!decision){
-       return;
-   }
+btnDelete.click(function() {
+    let decision = confirm("Jeste li sigurni da želite izbrisati korisnika?");
+    if (!decision) {
+        return;
+    }
     $.ajax({
         url: userUrl,
         type: 'DELETE'
-    })
-    ;
+    });
 });
 
 btnEdit.click(() => window.location.href = `${userUrl}edit`);
+
+const petValidation = {
+    name: {
+        identifier: 'pet-name',
+        rules: [{
+            type: 'empty',
+            prompt: 'Ljubimac mora imati ime'
+        }]
+    },
+    age: {
+        identifier: 'pet-age',
+        rules: [{
+            type: 'empty',
+            prompt: 'Ljubimac mora imati broj godina'
+        }]
+    },
+    species: {
+        identifier: 'pet-species',
+        rules: [{
+            type: 'empty',
+            prompt: 'Molimo odaberite vrstu ljubimca'
+        }]
+    }
+};
+
+$(document)
+    .ready(function() {
+        $('.ui.form').form({
+            inline: false,
+            fields: petValidation
+        });
+    });
+

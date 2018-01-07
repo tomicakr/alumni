e9eaffd3e2acde485d748643d43e59ddd8713832
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-01-07 21:08:27 +0100

    resCon

--- src/main/java/hr/petsonly/web/ReservationController.java
+++ src/main/java/hr/petsonly/web/ReservationController.java
@@ -6,9 +6,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
-import org.hibernate.hql.internal.ast.tree.FromElementFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;

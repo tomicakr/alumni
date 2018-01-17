31dd91e17af8273b9cd46ca3ff8d6cc805aa5a51
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-01-17 14:14:12 +0100

    malo poboljsan security, dodano springovo provjeravanje pristupa korisnicima

--- src/main/java/hr/petsonly/WebSecurityConfig.java
+++ src/main/java/hr/petsonly/WebSecurityConfig.java
@@ -1,14 +1,18 @@
 package hr.petsonly;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import hr.petsonly.config.specific.CustomAuthenticationSuccesHandler;
+import hr.petsonly.model.details.CustomUserDetails;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.authorizeRequests().antMatchers("/users").hasRole("ADMINISTRATOR").antMatchers("/jobs").hasAnyRole("ZAPOSLENIK", "ADMINISTRATOR");
 		
+		http.authorizeRequests().antMatchers("/users/{id}").access("@webSecurityConfig.checkUserId(authentication, #id)");
+		
 		http.csrf().disable();
 		
 	}
@@ -39,6 +45,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder builder) throws Exception {
 		builder.userDetailsService(userDetailsService);
+	}
+	
+	public boolean checkUserId(Authentication auth, UUID id) {
+		Object o = auth.getPrincipal();
+		if (!(o instanceof CustomUserDetails)) return false;
+		
+		CustomUserDetails user = (CustomUserDetails) o;
+		
+		if(user.getRoles().contains("ROLE_ADMINISTRATOR")) return true;
+		if(!user.getUserId().equals(id)) return false;
+		
+		return true;
 	}
 
 }

9b33e6211a3a4e81a5a6f852371933df8f8e3325
Mate Paulinovic <mate.paulinovic@fer.hr>
2018-01-21 15:02:53 +0100

    Dodane klase koje su falile

--- src/main/java/hr/petsonly/DemoApplication.java
+++ src/main/java/hr/petsonly/DemoApplication.java
@@ -2,13 +2,16 @@ package hr.petsonly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
 }
